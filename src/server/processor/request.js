70a9f356ea10472de4c8efc61b8e50780d42a041
signalwerk <sh@signalwerk.ch>
2024-12-28 23:52:28 +0100

    ADD: isPathValid function

--- src/server/processor/request.js
+++ src/server/processor/request.js
@@ -1,3 +1,37 @@
+export function isPathValid({ job, allowed, disallowed }, next) {
+  let path = null;
+  try {
+    const parsedUrl = new URL(job.data.uri);
+    path = parsedUrl.pathname + parsedUrl.search;
+  } catch (error) {
+    throw new Error("Error occurred while parsing the URL.");
+  }
+
+  // Check disallowed path-patterns
+  if (
+    disallowed?.some((pattern) =>
+      pattern instanceof RegExp ? pattern.test(path) : pattern === path,
+    )
+  ) {
+    job.log(`Domain ${path} is disallowed.`);
+    return next(null, true);
+  }
+
+  // Check allowed path-patterns
+  if (
+    allowed.length === 0 ||
+    allowed.some((pattern) =>
+      pattern instanceof RegExp ? pattern.test(path) : pattern === path,
+    )
+  ) {
+    job.log(`Domain ${path} is allowed.`);
+    return next();
+  }
+
+  job.log(`Domain ${path} is not allowed.`);
+  next(null, true);
+}
+
 export function isDomainValid({ job, allowed, disallowed }, next) {
   let domain = null;
   try {
@@ -7,7 +41,7 @@ export function isDomainValid({ job, allowed, disallowed }, next) {
     throw new Error("Error occurred while parsing the URL.");
   }
 
-  // Check disallowed domains/patterns
+  // Check disallowed domain-patterns
   if (
     disallowed?.some((pattern) =>
       pattern instanceof RegExp ? pattern.test(domain) : pattern === domain,
@@ -17,7 +51,7 @@ export function isDomainValid({ job, allowed, disallowed }, next) {
     return next(null, true);
   }
 
-  // Check allowed domains/patterns
+  // Check allowed domain-patterns
   if (
     allowed.length === 0 ||
     allowed.some((pattern) =>
